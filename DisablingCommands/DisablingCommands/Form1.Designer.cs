6df9f66fa7f854b55fe86a3a71f52d50a3245893
Muhammad Khuzaima Umair <75787251+mkhuzaima@users.noreply.github.com>
2021-08-02 22:21:56 +0500

    detected the changes in clipboard
    Blocked (Ctrl + X, Ctrl + C, Ctrl + V) and detected the clipboard using {SharpClipBoard}

---
+++ DisablingCommands/DisablingCommands/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace DisablingCommands
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.sharpClipboard1 = new WK.Libraries.SharpClipboardNS.SharpClipboard(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(34, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(361, 369);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // sharpClipboard1
+            // 
+            this.sharpClipboard1.MonitorClipboard = true;
+            this.sharpClipboard1.ObservableFormats.All = true;
+            this.sharpClipboard1.ObservableFormats.Files = true;
+            this.sharpClipboard1.ObservableFormats.Images = true;
+            this.sharpClipboard1.ObservableFormats.Others = true;
+            this.sharpClipboard1.ObservableFormats.Texts = true;
+            this.sharpClipboard1.ObserveLastEntry = true;
+            this.sharpClipboard1.Tag = null;
+            this.sharpClipboard1.ClipboardChanged += new System.EventHandler<WK.Libraries.SharpClipboardNS.SharpClipboard.ClipboardChangedEventArgs>(this.sharpClipboard1_ClipboardChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Copy or cut the text to see the magic ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(430, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(358, 434);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 478);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private WK.Libraries.SharpClipboardNS.SharpClipboard sharpClipboard1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ DisablingCommands/DisablingCommands/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using WK.Libraries.SharpClipboardNS;
+using WK.Libraries.SharpClipboardNS;
+
+namespace DisablingCommands
+{
+    public partial class Form1 : Form
+    {
+        //private Keys[] DisabledKeys;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void AddDisableKeys()
+        //{
+        //    Keys copyCmd = Keys.Control | Keys.C;
+        //    Keys cutCmd = Keys.Control | Keys.X;
+        //    Keys pasteCmd = Keys.Control | Keys.V;
+        //    Keys lStartBtn = Keys.LWin;
+        //    Keys rStartBtn = Keys.RWin;
+        //    Keys altF4Cmd = Keys.Alt | Keys.F4;
+        //    DisabledKeys = new Keys[] { copyCmd, cutCmd, pasteCmd, lStartBtn, rStartBtn, altF4Cmd };
+        //}
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            Keys[] disabledKeys = new Keys[]
+            {
+                Keys.Control | Keys.C,
+                Keys.Control | Keys.X,
+                Keys.Control | Keys.V,
+                Keys.Alt | Keys.F4
+            };
+            if (disabledKeys.Contains(keyData))
+            {
+                return true;
+            }
+            return false;
+        }
+        /**********************************************************************************/
+
+        //helping link: https://github.com/Willy-Kimura/SharpClipboard
+        //https://stackoverflow.com/questions/621577/how-do-i-monitor-clipboard-changes-in-c
+
+        private void sharpClipboard1_ClipboardChanged(object sender, SharpClipboard.ClipboardChangedEventArgs e)
+        {
+            // Is the content copied of text type?
+            if (e.ContentType == SharpClipboard.ContentTypes.Text)
+            {
+                // Get the cut/copied text.
+               richTextBox1.Text += "--\n--" +sharpClipboard1.ClipboardText;
+                
+            }
+
+            // Is the content copied of image type?
+            else if (e.ContentType == SharpClipboard.ContentTypes.Image)
+            {
+                // Get the cut/copied image.
+                Image img = sharpClipboard1.ClipboardImage;
+                pictureBox1.Image = img;
+            }
+
+            // Is the content copied of file type?
+            else if (e.ContentType == SharpClipboard.ContentTypes.Files)
+            {
+                // Get the cut/copied file/files.
+               richTextBox1.Text += "--\n--" +sharpClipboard1.ClipboardFiles.ToArray();
+
+                // ...or use 'ClipboardFile' to get a single copied file.
+               richTextBox1.Text += "--\n--" +sharpClipboard1.ClipboardFile;
+            }
+
+            // If the cut/copied content is complex, use 'Other'.
+            else if (e.ContentType == SharpClipboard.ContentTypes.Other)
+            {
+                // Do something with 'sharpClipboard1.ClipboardObject' or 'e.Content' here...
+            }
+        }
+
+        /**********************************************************************************/
+    }
+}
+
+
+
