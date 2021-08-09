c0d5f77c924002684889727b58919966fc3713a6
Muhammad-Ali-Murtaza <81867736+Muhammad-Ali-Murtaza@users.noreply.github.com>
2021-08-09 12:17:07 +0500

    Sleep detector and USB detector uploaded

---
+++ SleepDetector/Form1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SleepDetector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            SystemEvents.PowerModeChanged += OnPowerChange;
+
+        }
+        private void OnPowerChange(object s, PowerModeChangedEventArgs e)
+        {
+            System.Speech.Synthesis.SpeechSynthesizer sythesizer = new System.Speech.Synthesis.SpeechSynthesizer();
+            switch (e.Mode)
+            {
+                case PowerModes.StatusChange:
+                    sythesizer.Speak("System charging status is changing");
+
+                    break;
+                case PowerModes.Suspend:
+                    sythesizer.Speak("System is going to sleep");
+                    break;
+                case PowerModes.Resume:
+                    //MessageBox.Show("I am awaiking the system");
+                    MessageBox.Show(sythesizer.ToString());
+                    if (sythesizer == null)
+                    {
+                        //MessageBox.Show("In condition");
+                    sythesizer = new System.Speech.Synthesis.SpeechSynthesizer();
+                    }
+                    //Thread.Sleep(1000);
+                    sythesizer.Speak("System is awaking");
+
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("ms-settings:clipboard");
+        }
+    }
+}
---
+++ USBDetector/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USBDetector
+{
+    class Program
+    {
+        //DriveInfo[] allDrives = DriveInfo.GetDrives();
+        static void Main(string[] args)
+        {
+            USBdetector();
+        }
+
+        public static bool USBdetector()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            bool flag = false;
+            Console.WriteLine(allDrives);
+            foreach (DriveInfo d in allDrives)
+            {
+
+                if (d.DriveType == (DriveType)2)
+                {
+
+                    if (d.IsReady == true)
+                    {
+                    synth.Speak("REmoveable device name " + d.Name + " is found");
+                        Console.WriteLine("Drive {0}", d.Name);
+                        Console.WriteLine("  Drive type: {0}", d.DriveType);
+                        Console.WriteLine("  Volume label: {0}", d.VolumeLabel);
+                        Console.WriteLine("  File system: {0}", d.DriveFormat);
+                        Console.WriteLine(
+                            "  Available space to current user:{0, 15} bytes",
+                            d.AvailableFreeSpace);
+
+                        Console.WriteLine(
+                            "  Total available space:          {0, 15} bytes",
+                            d.TotalFreeSpace);
+
+                        Console.WriteLine(
+                            "  Total size of drive:            {0, 15} bytes ",
+                            d.TotalSize);
+                    }
+                    flag = true;
+                    //return flag;
+                }
+            }
+            if (!flag)
+            {
+                synth.Speak("No removeable device found");
+                return false;
+            }
+            return true;
+        }
+    }
+}
