7059427c2a2e82b765953fcfe0dcfe3968b1c82f
Muhammad-Ali-Murtaza <muhammadalimurtaza997@gmail.com>
2021-09-10 00:03:38 -0700

    New Project of Network Detection with event changing

---
+++ NeworkDetectorWithEventHandler/NetworkConnection/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace NetworkConnection
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkFunction()
+        {
+            //NetworkChange.NetworkAvailabilityChanged += new NetworkAvailabilityChangedEventHandler((sender, e) => AvailabilityChangedCallback(sender, e));
+
+        }
+
+        private void AvailabilityChangedCallback(object sender, NetworkAvailabilityEventArgs e)
+        {
+            synth.Speak("I am in function of network checking");
+            if (e.IsAvailable)
+            {
+                //panel1.ForeColor = Color.Green;
+                panel2.BackColor = Color.Green;
+                synth.Speak("Network is available");
+                //Internet Connection is available   
+            }
+            else
+            {
+                panel2.BackColor = Color.Red;
+                synth.Speak("Network is not available");
+            }
+        }
+
+        private void button1_Click(object sender1, EventArgs e1)
+        {
+            //synth.Speak("I am going to check network connection");
+            NetworkChange.NetworkAvailabilityChanged += new NetworkAvailabilityChangedEventHandler((sender, e) => AvailabilityChangedCallback(sender, e));
+        }
+    }
+}
